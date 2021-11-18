c212e2e597d7850901559cdf9c59d77792bdd881
NXTGenforcer <gskywalker11@gmail.com>
2021-11-18 18:27:20 -0500

    commit

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,28 +1,98 @@
 var userInput = "";
 var apiKey = "1fa8f8c718d4c529efbc4c2173fb3060";
 var baseURL = "https://api.openweathermap.org/data/2.5/weather?q="; //{city name}&appid={API key}
-var city = "Nashville";
 var requestUrl = baseURL + userInput + "&appid=" + apiKey;
+var userSearch;
+var now = moment();
+var todayDate = now.format("YYYY-MM-DD");
+var fifthDay = now.add(6, "days").format("YYYY-MM-DD");
 
 $(document).ready(function () {
   $(".btn").on("click", function (event) {
     event.preventDefault();
+
     userInput = $(".form-control").val();
-    console.log(userInput);
-    localStorage.setItem("Search", JSON.stringify(userInput));
+    localStorage.setItem("search", JSON.stringify(userInput));
+    $(".form-control").val("");
+    userSearch = JSON.parse(localStorage.getItem("search"));
+    var requestUrl = baseURL + userSearch + "&appid=" + apiKey;
     if (!userInput) {
       alert("Must input a city's name to continue");
+    } else {
+      clear();
+      render();
     }
-    // // else {
-    //   location.replace("results.html");
-    // }
-    var requestUrl = baseURL + userInput + "&appid=" + apiKey;
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-      });
   });
 });
+function render() {
+  var requestUrl = baseURL + userSearch + "&appid=" + apiKey;
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      fetch(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=${apiKey}`
+      )
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          var temp = (data.current.temp - 273.15) * 1.8 + 32;
+          var F = Math.trunc(temp);
+          var wind = data.current.wind_speed;
+          var humid = data.current.humidity;
+          var UVI = data.current.uvi;
+          var today = $(".daily");
+          var searchList = $(".prevCities");
+          var fiveDay = $(".forecast-cards");
+          var list = $(".list-group");
+          var bigCard = `
+              <div class="card city-day border-dark">
+                ${userInput}(${todayDate})
+                <img src="https://openweathermap.org/img/w/${data.current.weather[0].icon}.png">
+                
+                  Temp: ${F}
+                  Wind: ${wind}
+                  Humidity: ${humid}
+                  UV Index: ${UVI}
+              </div>`;
+          data.daily.forEach((day) => {
+            var currentDay = moment.unix(day.dt).format("YYYY-MM-DD");
+            if (moment(currentDay).isBetween(todayDate, fifthDay)) {
+              var dayTemp = (day.temp.day - 273.15) * 1.8 + 32;
+              var dayF = Math.trunc(dayTemp);
+              console.log(dayF, dayTemp);
+              var card = `
+                  <div class="card per-day"> 
+                  ${currentDay}
+                  <img src="https://openweathermap.org/img/w/${day.weather[0].icon}.png">
+                    Temp:${dayF} F
+                    Wind:${day.wind_gust} MPH 
+                    Humidity:${day.humidity} %
+                  </div>`;
+              fiveDay.append(card);
+            }
+          });
+          var prevSearch = ` <div class="col">
+              <button class="btn prevBtn" type="submit" id="prevBtn">${userInput}</button>
+                      </div>`;
+          today.append(bigCard);
+          searchList.append(prevSearch);
+        });
+    });
+}
+
+$("body").on("click", ".prevBtn", function (event) {
+  event.preventDefault();
+  userInput = $("body").$(".prevBtn").textContent();
+  clear();
+  render();
+});
+
+function clear() {
+  $(".daily").empty();
+  $(".forecast-cards").empty();
+  $("list-group list-group-flush").empty();
+}
