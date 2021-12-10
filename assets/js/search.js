397153b306f0f11dea93caad501b0096bb96ccdb
NXTGenforcer <gskywalker11@gmail.com>
2021-12-10 18:07:02 -0500

    fixed a lot, pushing for carl

--- assets/js/search.js
+++ assets/js/search.js
@@ -6,26 +6,29 @@ var userSearch;
 var now = moment();
 var todayDate = now.format("YYYY-MM-DD");
 var fifthDay = now.add(6, "days").format("YYYY-MM-DD");
+var searchArray = JSON.parse(localStorage.getItem("search")) || [];
 
 $(document).ready(function () {
-  $(".btn").on("click", function (event) {
+  $(".searchcity").on("click", function (event) {
     event.preventDefault();
-
-    userInput = $(".form-control").val();
-    localStorage.setItem("search", JSON.stringify(userInput));
-    $(".form-control").val("");
+    if (!searchArray.includes($(".form-control").val())) {
+      searchArray.push($(".form-control").val());
+    }
+    localStorage.setItem("search", JSON.stringify(searchArray));
     userSearch = JSON.parse(localStorage.getItem("search"));
-    var requestUrl = baseURL + userSearch + "&appid=" + apiKey;
-    if (!userInput) {
+    if (!$(".form-control").val()) {
       alert("Must input a city's name to continue");
     } else {
       clear();
-      render();
+      render(userSearch);
     }
   });
+  for (let i = 0; i < searchArray.length; i++) {
+    btnRender(searchArray[i]);
+  }
 });
-function render() {
-  var requestUrl = baseURL + userSearch + "&appid=" + apiKey;
+function render(search) {
+  requestUrl = baseURL + search + "&appid=" + apiKey;
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
@@ -50,7 +53,7 @@ function render() {
           var list = $(".list-group");
           var bigCard = `
               <div class="card city-day border-dark">
-                ${userInput}(${todayDate})
+                ${search}(${todayDate})
                 <img src="https://openweathermap.org/img/w/${data.current.weather[0].icon}.png">
                 
                   Temp: ${F}
@@ -75,21 +78,25 @@ function render() {
               fiveDay.append(card);
             }
           });
-          var prevSearch = ` <div class="col">
-              <button class="btn prevBtn" type="submit" id="prevBtn">${userInput}</button>
-                      </div>`;
+          if (!searchArray.includes(search)) {
+            var prevSearch = btnRender(search);
+            searchList.append(prevSearch);
+          }
           today.append(bigCard);
-          searchList.append(prevSearch);
         });
     });
 }
 
-$("body").on("click", ".prevBtn", function (event) {
-  event.preventDefault();
-  userInput = $("body").$(".prevBtn").textContent();
+function btnRender(search) {
+  return ` <div class="col">
+  <button class="btn prevBtn" type="submit" id="prevBtn" onclick="newSearch('${search}')">${search}</button>
+          </div>`;
+}
+
+function newSearch(city) {
   clear();
-  render();
-});
+  render(city);
+}
 
 function clear() {
   $(".daily").empty();
